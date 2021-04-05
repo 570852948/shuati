26cf0397ec425c02bb462c45e2c05870055e49ba
东风客 <570852948@qq.com>
2021-04-05 14:02:57 +0800

    测试

--- src/main/java/Test/Default.java
+++ src/main/java/Test/Default.java
@@ -3,6 +3,6 @@ package Test;
 public interface Default {
     default void o() {
         System.out.println(this.getClass());
-        System.out.println("o方法");
+        System.out.println("o方法测试");
     }
 }
